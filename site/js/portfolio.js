d977409943a3ddbbdd298241f99fb8d546cd8fbc
AxelCabanat <axelcabanat@gmail.com>
2021-10-15 12:58:47 +0200

    complete part 3 except the JS Super Bonus

---
+++ site/js/portfolio.js
@@ -0,0 +1,37 @@
+const profilPic = document.getElementById("profilPic")
+profilPic.addEventListener("click", ()=>{
+    profilPic.src = "image/avatar.svg";
+
+})
+
+const nameButton = document.getElementById("nameButton")
+const userName = document.getElementById("name")
+const userBG = document.querySelectorAll(".pink-bg")
+const userText = document.querySelectorAll(".pink-text")
+const userLink = document.querySelectorAll("a")
+
+
+nameButton.addEventListener("click", ()=>{
+    userName.innerHTML= prompt("What's your name ?");
+    userName.style.color = "white";
+    const userColor = prompt("Choose a color");
+    userBG.forEach(element => {
+        element.style.backgroundColor = userColor;        
+    });
+    userText.forEach(element => {
+        element.style.color = userColor;        
+    });
+    userLink.forEach(element => {
+        element.style.color = userColor;        
+    });
+})
+const liButton = document.getElementById("clickme")
+const firstLi = document.getElementById("firstLi")
+const secondLi = document.getElementById("secondLi")
+const thirdLi = document.getElementById("thirdLi")
+
+liButton.addEventListener("click", () => {
+    firstLi.innerHTML = "VS Code";
+    secondLi.innerHTML = "Git";
+    thirdLi.innerHTML = "Terminal";
+})
