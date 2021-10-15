7a382607f6052b9d7ce537be163c1ecdfbf3aea6
ArthurG1000 <garnierarthur.contact@gmail.com>
2021-10-15 12:10:10 +0200

    Added corrections on parentheses

--- algo/2/isLeapYear.js
+++ algo/2/isLeapYear.js
@@ -12,11 +12,11 @@ Autre rappel: Tu dois utiliser l'opérateur modulo (%) pour vérifier si un nomb
 
 function isLeapYear(year) {
 
-  if ((year%4===0) && ((year%100!=0) || (year%400===0))) 
+  if (((year%4===0) && (year%100!=0)) || (year%400===0))
       return true;
 
-  else {
-      return false;
+      else {
+        return false;
 }};
 
 
