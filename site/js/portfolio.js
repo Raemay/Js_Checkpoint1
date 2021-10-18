5c7ec5508bdaa3b62d28dcfe16cb30305f05acbe
Mathieu Cachen <mathieu.cachen@gmail.com>
2021-10-18 09:37:57 +0200

    finished 4th task js for checkpoint

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -11,7 +11,16 @@ function changeAvatar() {
 /*MODIFIER TEXTE+COULEUR INPUT */
 function modifyTextAndColor() {
     const userName = prompt("Enter your name:");
+    const userColor = prompt("Enter your color:");
     document.querySelector('#name').style.color = 'white';
     document.querySelector('#name').textContent = userName;
     document.querySelector('.pink-bg').style.background = '#750ff7';
+    const colorBackground = document.querySelectorAll('.pink-bg')
+        for (let i = 0; i < colorBackground.length; i++) {
+            colorBackground[i].style.background = userColor;
+        }
+    const colorText = document.querySelectorAll('.pink-text');
+        for (let i = 0; i < colorText.length; i++) {
+            colorText[i].style.color = userColor;
+        }
 }
