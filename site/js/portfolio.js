6acb469e8e59daea8d4d948f7000340785b77402
Mahfod <mahfod.dev@gmail.com>
2022-01-11 21:12:04 +0100

    add prompt to change color

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -3,6 +3,9 @@ console.log('hello')
 const avatar = document.querySelector('.avatar')
 const nameGreet = document.getElementById('name')
 const button = document.querySelector('button')
+const description = document.querySelector('.pink-bg')
+
+console.log(description)
 
 avatar.addEventListener('click', () => {
 	avatar.src = '../image/avatar.svg'
@@ -10,5 +13,8 @@ avatar.addEventListener('click', () => {
 console.log(nameGreet)
 button.addEventListener('click', () => {
 	const enterName = prompt('Enter your name')
+	const changeColor = prompt('Enter your color')
+	console.log(changeColor)
 	nameGreet.textContent = enterName
+	description.style.backgroundColor = changeColor
 })
