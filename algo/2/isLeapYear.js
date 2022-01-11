624a6fb89fc300705b1a05727a1c042981292d8b
Agathe Geniteau <geniteau@lemonde.fr>
2022-01-11 20:48:48 +0100

    add the result of algo2

--- algo/2/isLeapYear.js
+++ algo/2/isLeapYear.js
@@ -10,6 +10,14 @@ Autre rappel: Tu dois utiliser l'opérateur modulo (%) pour vérifier si un nomb
 */
 
 function isLeapYear(year) {
+  if ((year%4==0 && year%100!=0) || (year%400==0)) {
+  return true
+  }
+  else{
+    return false
+  };
+  
+ 
   // TODO: Your code here
 }
 
