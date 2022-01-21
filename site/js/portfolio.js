0a92d7e656629e2cb9a1074f1c2917a7ead29968
frederic andolina <fredericandolina@macbook-air-de-frederic.home>
2022-01-21 10:53:18 +0100

    Exercice2 js change name and color

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,4 +1,12 @@
 function changeAvatar(){
     const imageAvatar2 =document.querySelector(".imageAvatar")
-    imageAvatar2.src = "./image/avatar-bis.png"
+    imageAvatar2.src = "./image/avatar.svg"
+}
+
+function modifyName(){
+    const newName =document.querySelector("#name")
+    const question = prompt("Enter your Name:")
+    newName.innerHTML= question
+    newName.style.color= "white"
+    
 }
