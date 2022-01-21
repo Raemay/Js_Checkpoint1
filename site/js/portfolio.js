abf7728f190ef19cf73d294e3b96d7db0ef0e3d1
JeanMichel999 <93377201+JeanMichel999@users.noreply.github.com>
2022-01-21 17:33:40 +0100

    version5

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,15 +1,51 @@
 /*Fonction avatar*/
-const avatar = ()=>{
+const avatar = () => {
     const image = document.querySelector('#avatar');
-    image.src="image/avatar.svg"
+    image.src = "image/avatar.svg"
 }
 /*Fonction button*/
-const button = ()=>{
+const button = () => {
     const nom = prompt("Entrez votre nom:");
     const couleur = prompt("Changer la couleur");
     const name = document.querySelector("#name")
-    const color = document.querySelector(".pink-bg")
+    const color = document.querySelectorAll(".pink-bg")
+    const text = document.querySelectorAll(".pink-text")
+    const lien = document.querySelectorAll("a")
     name.style.color = "white"
     name.innerHTML = nom
-    color.style.backgroundColor = couleur
+    /*boucle pour les 4 textes */
+    for (var i = 0, l = text.length; i < l; i++) {
+        text[i].style.color = couleur
+    }
+    /*boucle pour le fond et description*/
+    for (var i = 0, l = color.length; i < l; i++) {
+        color[i].style.backgroundColor = couleur
+    }
+    /*boucle pour le fond et description*/
+    for (var i = 0, l = lien.length; i < l; i++) {
+        lien[i].style.color = couleur
+    }
+}
+/*Bonus 2*/
+/*Fonction modif*/
+const bonusJMP = () => {
+    const bonus = document.querySelectorAll("li");
+    for (var i = 0, i = bonus.length; i < l; i++) {
+        bonus[i].style.color = couleur
+    }
+}
+
+/*bonus 3*/
+const resetform = () => {
+    const titre = document.querySelector("#depot_projet").value=""
+}
+
+
+const soumettre = (event) => {
+    event.preventDefault();
+    const titre = document.querySelector("#depot_projet").value
+    const creatLi = document.createElement("li")
+    creatLi.classList.add("li")
+    creatLi.innerHTML = `${titre}`
+    resetform()
 }
