0041f985387d28803cc56d923889560494ea99c4
antoine carpentier <a.carpentier.wcs@gmail.com>
2022-06-01 16:16:10 +0200

    js en cours 2

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -3,4 +3,22 @@ changepicture.onclick = function(){
     changepicture.src = "image/avatar.svg";
 };
 
-let changename = document.getElementById('#name')
+/*je n'ai pas fait la fonction seul, j'ai eu de l'aide*/
+
+let changename = document.querySelector(".btn")
+changename.addEventListener("click",() =>{
+    const rename = prompt("entrer votre prenom");
+    const recolor = prompt("entrer votre couleur");
+    let name = document.getElementById("name");
+    name.innerHTML = rename;
+    name.style.color = "white";
+
+    let color = document.querySelectorAll(".pink-bg");
+    color.forEach(color => color.style.backgroundColor = recolor);
+
+    let col = document.querySelectorAll(".pink-text")
+    col.forEach(col => col.style.color = recolor)
+
+    let colo = document.querySelectorAll("a")
+    colo.forEach(colo => colo.style.color = recolor ) 
+})
