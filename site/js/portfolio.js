3354633659a4e17c593a7c254b29d38211434f2f
Marina Alves <ad.marina@hotmail.fr>
2022-06-01 15:23:28 +0200

    js terminé sauf bonus

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -16,8 +16,12 @@ button.addEventListener("click", () => {
     name.innerHTML = request;
     name.style.color = "white";
 
-    const chang_color = document.querySelector(".pink-bg");
+    const chang_color = document.querySelectorAll(".pink-bg");
     const color1 = prompt("Choisissez une couleur: ");
-    chang_color.style.backgroundColor = color1;
+    chang_color.forEach(element => element.style.backgroundColor = `${color1}`);
+
+    const change_color2 = document.querySelectorAll(".pink-text");
+    console.log(change_color2);
+    change_color2.forEach(element => element.style.color = `${color1}`);
 
 })
