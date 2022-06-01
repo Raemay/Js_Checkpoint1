d7477952df70b7893aa3c7ee44c9e94ac937e263
Uriel SAUTRON <uriel.sautron@gmail.com>
2022-06-01 09:59:51 +0200

    Algo 6 done

--- algo/6/sumArr.js
+++ algo/6/sumArr.js
@@ -11,7 +11,9 @@ sumArr( ["2", "7", "3", "8", "2"], ["2", "4", "9"] ) doit renvoyer ["4", "11", "
 */
 
 function sumArr(arrayA, arrayB) {
-  // Your code here !
+  return arrayA.map((num, index) => {
+    return (Number(num) + Number(arrayB[index] || 0)).toString();
+  });
 }
 
 module.exports = sumArr;
