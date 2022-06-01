9c5290fe8fe23b6c33db30ff8c25df16a8ee7fec
AndreaCastel <marion.chateau@sncf.fr>
2022-06-01 14:24:37 +0200

    sauvegarde JS exo 1 et 2

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,14 +1,30 @@
+//changement d'avatar
 const newAvatar = document.querySelector(".avatar");
    newAvatar.onclick = function(){
        newAvatar.src="image/avatar.svg";
    };
    
+//changement de nom et couleur de nom
 
 const newName = document.getElementById("name");
     newName.textContent = 'Marion';
     newName.style.color = 'white';
 
+//remplacement du background color et du nom du bouton
+function promptbtn(){
+    let name = prompt("Please write your name");
+    };
 
-const buttonColor = document.createElement('Button');
-const newDiv = document.querySelector('.presentation');
-newDiv.appendChild(buttonColor);
+
+const presentation = document.getElementsByClassName("presentation");
+
+function changeColor(){
+    let btnName = document.querySelector('button');
+    btnName.addEventListener(
+        'click',
+        presentation.style.backgroundcolor = 'blue',
+        )
+    }
+
+   // const el = document.querySelector("#outside");
+   // el.addEventListener("click", modifyText, false);
