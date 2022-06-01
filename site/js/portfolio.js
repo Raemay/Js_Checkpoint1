43b6e1580f343b18aa173f55d7f056cab23f68f7
vanie28 <sylvanie.giraudt@sncf.fr>
2022-06-01 15:21:55 +0200

    CSS check

---
+++ site/js/portfolio.js
@@ -0,0 +1,10 @@
+/* addEventListener au ("click") sur l'image.
+changer la src via le DOM
+quel élément DOM?:
+*/
+
+/*Changement d'avatar*/
+document.querySelector(".banner1").addEventListener("click", function() {
+
+})
+
