3116721c21488e758ad307d16227436e790a8375
vanie28 <sylvanie.giraudt@sncf.fr>
2022-06-02 15:15:37 +0200

    portfolio changed

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -4,7 +4,37 @@ quel élément DOM?:
 */
 
 /*Changement d'avatar*/
-document.querySelector(".banner1").addEventListener("click", function() {
+let newAvatar = document.getElementById("avatar")
 
-})
+newAvatar.onclick = function (){
+  let mySrc = newAvatar.getAttribute("src")
+    if(mySrc === "image/avatar-bis.png"){
+       newAvatar.setAttribute("src", "image/avatar.svg");
+   }else{
+    newAvatar.setAttribute("src", "image/avatar-bis.png");
+   }
+}
+
+/*Changer la couleur 
+1)clic sur bouton et faire apparaitre un prompt*/
+const newColor = document.querySelector("#changeColor");
+
+newColor.addEventListener("onclick", () => {
+    const name = document.getElementById("name");
+    const button = prompt("enter your name");
+    name.innerHTML = button;
+    /*2)Résultat du prompt change le nom id:name et la couleur en blanc*/
+    name.style.color = "white";
+
+ /*3) Change la couleur partout class: pink-bg */
+    const chang_color = document.querySelectorAll(".pink-bg");
+    const color1 = prompt("choose your color: ");
+    chang_color.forEach(element => element.style.backgroundColor = `${color1}`);
+    const change_color2 = document.querySelectorAll(".pink-text");
+    change_color2.forEach(element => element.style.color = `${color1}`);
+});
+
+
+
+
 
