71071ef5b7c83f04d09e1eb46dd65a585e3e2c0e
nicolascarrere.pro@gmail.com <nicolascarrere.pro@gmail.com>
2022-10-10 09:24:27 +0200

    algo1 ok, function is working correctly

--- algo/1/thirdAngle.js
+++ algo/1/thirdAngle.js
@@ -8,6 +8,9 @@ thirdAngle(20, 80) doit renvoyer 80
 
 function thirdAngle(a, b) {
   // Your code here !
+  return a + b < 180
+    ? 180 - a - b
+    : `les valeurs de a & b sont trop élevées. veuillez vérifier vos valeurs saisies et recommencer.`;
 }
-
+console.log(thirdAngle(20, 180));
 module.exports = thirdAngle;
